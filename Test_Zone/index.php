62c5eaad2fead28af1d5425bdc01f32a6b7888c4
ZigorewsLike <30562858+ZigorewsLike@users.noreply.github.com>
2018-02-08 16:02:01 +0800

    Create index.php

---
+++ Test_Zone/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<Html>
+<head>
+<meta charset="UTF-8">
+ <title>Отзывы</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+ <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+  <link href='style.css' rel='stylesheet' type='text/css'>
+  <div class="container">
+  <div class="row">
+  <img src="header.jpg" width=100%>
+  </div>
+<nav class="navbar navbar-default row">
+<ul class="nav nav-pills">
+  <li role="presentation" class="active nav-items"><a href="#">Главная</a></li>
+  <li role="presentation dropdown" class="nav-items"><a href="" class="dropdown-toggle" data-toggle="dropdown">Услуги
+  <span class="glyphicon glyphicon glyphicon-align-justify" aria-hidden="true"></span></a></li>
+  <li role="presentation" class="nav-items"><a href="#">Прайсы</a></li>
+  <li role="presentation" class="nav-items"><a href="#">Контакты</a></li>  </ul></nav>
+</div>
+ </head> <!-- конец тега head и подключения библиотек для других модулей или веб страниц. -->
+ <body>
+
+ <div class="container">
+
+<?php include "content.php";?>
+
+<!-- БЛОК 2 -->
+
+<!-- FOOTER. Ну мало ли ты запутаешся в своём мусоре (коде) -->  
+<footer class="panel panel-footer">
+тут всякая дичь. Обычно у меня тут счётчик поситителей и телефоны компании, организации и т.д.
+</footer></div></body>
+ </html>
