daaa220d2e220ecc7cc77092e035d579a9d2abce
ZigorewsLike <30562858+ZigorewsLike@users.noreply.github.com>
2018-10-29 02:06:45 +0800

    Update Lab4.java

--- Java/Lab1/src/lab1/Lab4.java
+++ Java/Lab1/src/lab1/Lab4.java
@@ -8,11 +8,15 @@ import java.util.*;
 import java.io.*;
 
 public class Lab4 {
-    public static void main(String[] args) throws Exception{
-        System.out.println("-Введите абсолютный путь до файла-");
+    public static void main(String[] args) throws IOException{
+        System.out.println("-Введите абсолютный путь до файла.-");
         Scanner dir = new Scanner(System.in);
+        System.out.println("-Куда сохранить? Введите абсолютный путь.-");
+        Scanner dirW = new Scanner(System.in);
         String path = dir.nextLine();
+        String pathW = dirW.nextLine();
         File myFile = new File(path);
+        File myFileW = new File(pathW);
          if(!myFile.exists()){
              System.out.println("-ОШИБКА! Файл не найден!-");
          }else{
@@ -21,8 +25,8 @@ public class Lab4 {
         int i = 1;
         String []textEx = new String [4]; 
         textEx[0] = "\\chapter";
-        textEx[2] = "\\section";
-        textEx[1] = "\\subsection";
+        textEx[1] = "\\section";
+        textEx[2] = "\\subsection";
         textEx[3] = "\\subsubsection";
         boolean []existText = new boolean[4];
         existText[0] = false;
@@ -31,14 +35,15 @@ public class Lab4 {
         existText[3] = false;
         
         System.out.println("-Результат (Вывод заголовков):-");
+        FileWriter writer = new FileWriter(myFileW); 
         while (scn.hasNextLine()) {
             String str = scn.nextLine();
             //System.out.println(i + " : '" + str + "' length = " + str.length());
-            for(int word=0;word<4;word++){
+            for(int word=3;word>-1;word--){
                 for(int j=0;j<str.length() - textEx[word].length();j++){
                     for(int k=0;k<textEx[word].length();k++){
                         if(textEx[word].charAt(k) == str.charAt(k+j)){
-                            existText[word] = true;;
+                            existText[word] = true;
                         }else{
                             existText[word] = false;
                         }
@@ -46,20 +51,33 @@ public class Lab4 {
                     int len = textEx[word].length();
                     if(existText[word]){
                         String newStr = "";
+                        for(int ii=0;ii<word+1;ii++){
+                            newStr += "  ";
+                            //System.out.println(word);
+                        }
+                        
                         for(int kk = j+len+1;kk<str.length()-1;kk++){
                             newStr += str.charAt(kk);
                         }
+                        newStr += "\r";
+                        writer.write(newStr);
+                        writer.append('\n');
+                        writer.flush();
                         System.out.println(newStr);
                         break;
                     }
                 }
                  if(existText[word]){
-                     break;
+                     existText[0] = false;
+                     existText[1] = false;
+                     existText[2] = false;
+                     existText[3] = false;
+                     break;                  
                  }
             }
             i++;
         }
-        
+        writer.close();
         file.close();
         }
     }
